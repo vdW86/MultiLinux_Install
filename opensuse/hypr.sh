cf1d0a7ca27affad59353afafb3b05ede591e649
Kevin <spamenpruttel@protonmail.com>
2026-05-26 17:30:30 +0200

    test

--- opensuse/hypr.sh
+++ opensuse/hypr.sh
@@ -1,7 +1,1 @@
-sudo zypper install --no-recommends hyprland 
-foot 
-waybar 
-swaybg
-grim
-slurp
-
+sudo zypper install --no-recommends hyprland foot waybar swaybg grim slurp dbus-1 xorg-x11-server-Xwayland mesa
