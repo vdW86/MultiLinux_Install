7a80ca55d164325240b94118370b63b4b11ab3cf
Kevin <spamenpruttel@protonmail.com>
2026-06-20 17:17:08 +0200

    test

---
+++ install_new.sh
@@ -0,0 +1,6 @@
+sudo zypper install --no-recommends \
+    git cmake meson ninja gcc-c++ \
+    wayland-devel libxkbcommon-devel libinput-devel libdrm-devel \
+    mesa-devel libglvnd-devel libseat-devel libudev-devel \
+    pipewire wireplumber xdg-desktop-portal-hyprland \
+    seatd polkit
